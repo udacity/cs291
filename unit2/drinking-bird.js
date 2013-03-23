0388c3ab2adc74b238352a10b892bb044ac43bec
Vaughn Dickson <vaughnd@gmail.com>
2013-03-23 16:41:12 +0200

    Change this code to match online course code.

--- unit2/drinking-bird.js
+++ unit2/drinking-bird.js
@@ -5,13 +5,13 @@
 // Hat: cylinder. color blue (cylinderMaterial)                               //
 //      Diameter top 80, bottom, full height 80, edge 10                      //
 // Head: sphere, red (sphereMaterial), diameter 104                           //
-// Middle of base: cube, color orange (cubeMaterial), width 77, length 194    //
-// Feet: cube, color orange, width 6, length 194, height 52                   //
-// Legs: cube, color orange, width 6, length 64, height 386                   //
+// Middle of base: cube, color orange (cubeMaterial), width 77, length 194     //
+// Feet: cube, color orange, width 6, length 194, height 52                    //
+// Legs: cube, color orange, width 6, length 64, height 386                    //
 // Body: sphere, red, diameter 116                                            //
 // Spine: cylinder, blue, diameter 24, length 390                             //
 ////////////////////////////////////////////////////////////////////////////////
-/*global THREE, Coordinates, document, window, dat*/
+/*global, THREE, Coordinates, $, document, window, dat*/
 
 var camera, scene, renderer;
 var cameraControls, effectController;
@@ -19,18 +19,38 @@ var clock = new THREE.Clock();
 var gridX = false;
 var gridY = false;
 var gridZ = false;
-var axes = true;
+var axes = false;
 var ground = true;
-// MATERIALS for the bird
-var sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
-var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xF07020 } );
-var cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
+
+function init() {
+	var canvasWidth = 846;
+	var canvasHeight = 494;
+	var canvasRatio = canvasWidth / canvasHeight;
+
+	// RENDERER
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.gammaInput = true;
+	renderer.gammaOutput = true;
+	renderer.setSize(canvasWidth, canvasHeight);
+	renderer.setClearColorHex( 0xAAAAAA, 1.0 );
+
+	// CAMERA
+	camera = new THREE.PerspectiveCamera( 45, canvasRatio, 1, 40000 );
+	// CONTROLS
+	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+
+	camera.position.set( -480, 659, -619 );
+	cameraControls.target.set(4,301,92);
+
+	fillScene();
+}
 
 // Supporting frame for the bird - base + legs + feet
 function createSupport() {
-	var cube;
-	
+
+   var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xF07020 } );
 	// base
+	var cube;
 	cube = new THREE.Mesh( 
 		new THREE.CubeGeometry( 20+64+110, 4, 2*77 ), cubeMaterial );
 	cube.position.x = -45;	// (20+32) - half of width (20+64+110)/2
@@ -57,27 +77,20 @@ function createSupport() {
 	// right foot
 	
 	// right leg
-	
+
 }
 
 // Body of the bird - body and the connector of body and head
 function createBody() {
-	var sphere, cylinder;
-
-	// body
-
-	// head/body connector (spine)
+   var sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
+   var cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
 
 }
 
 // Head of the bird - head + hat
 function createHead() {
-	var sphere, cylinder;
-	// head 
-
-	// hat brim
-	
-	// hat top
+   var sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
+   var cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
 
 }
 
@@ -95,12 +108,11 @@ function createDrinkingBird() {
 }
 
 function fillScene() {
+	// SCENE
 	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog( 0xAAAAAA, 2000, 4000 );
-
+	scene.fog = new THREE.Fog( 0x808080, 3000, 6000 );
 	// LIGHTS
 	var ambientLight = new THREE.AmbientLight( 0x222222 );
-
 	var light = new THREE.DirectionalLight( 0xffffff, 1.0 );
 	light.position.set( 200, 400, 500 );
 	
@@ -111,49 +123,25 @@ function fillScene() {
 	scene.add(light);
 	scene.add(light2);
 
+	scene.add(camera);
+
 	if (ground) {
-		Coordinates.drawGround({size:10000});		
+		Coordinates.drawGround({size:1000});		
 	}
 	if (gridX) {
-		Coordinates.drawGrid({size:10000,scale:0.01});
+		Coordinates.drawGrid({size:1000,scale:0.01});
 	}
 	if (gridY) {
-		Coordinates.drawGrid({size:10000,scale:0.01, orientation:"y"});
+		Coordinates.drawGrid({size:1000,scale:0.01, orientation:"y"});
 	}
 	if (gridZ) {
-		Coordinates.drawGrid({size:10000,scale:0.01, orientation:"z"});	
+		Coordinates.drawGrid({size:1000,scale:0.01, orientation:"z"});	
 	}
 	if (axes) {
 		Coordinates.drawAllAxes({axisLength:300,axisRadius:2,axisTess:50});
 	}
-	
 	createDrinkingBird();
 }
-
-function init() {
-	var canvasWidth = window.innerWidth;
-	var canvasHeight = window.innerHeight;
-	var canvasRatio = canvasWidth / canvasHeight;
-
-	// RENDERER
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.gammaInput = true;
-	renderer.gammaOutput = true;
-	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setClearColorHex( 0xAAAAAA, 1.0 );
-
-	// CAMERA
-	camera = new THREE.PerspectiveCamera( 45, canvasRatio, 1, 10000 );
-	camera.position.set( -500, 500, -1000 );
-	// CONTROLS
-	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
-	cameraControls.target.set(0,300,0);
-	
-	fillScene();
-
-}
-
-
 //
 function addToDOM() {
     var container = document.getElementById('container');
@@ -172,7 +160,6 @@ function animate() {
 function render() {
 	var delta = clock.getDelta();
 	cameraControls.update(delta);
-
 	if ( effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes)
 	{
 		gridX = effectController.newGridX;
@@ -194,7 +181,10 @@ function setupGui() {
 		newGridY: gridY,
 		newGridZ: gridZ,
 		newGround: ground,
-		newAxes: axes
+		newAxes: axes,
+
+		dummy: function() {
+		}
 	};
 
 	var gui = new dat.GUI();
