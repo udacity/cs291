d320fd4ff59f030384723fdc9d26f7a03d004221
Daniel P Pflager <daniel.pflager@gmail.com>
2020-07-27 19:45:57 -0700

    var -> let in drinking-bird.js

--- unit2/drinking-bird.js
+++ unit2/drinking-bird.js
@@ -18,22 +18,22 @@
 ////////////////////////////////////////////////////////////////////////////////
 /*global THREE, Coordinates, $, document, window, dat*/
 
-var camera, scene, renderer;
-var cameraControls, effectController;
-var clock = new THREE.Clock();
-var gridX = false;
-var gridY = false;
-var gridZ = false;
-var axes = false;
-var ground = true;
+let camera, scene, renderer;
+let cameraControls, effectController;
+let clock = new THREE.Clock();
+let gridX = false;
+let gridY = false;
+let gridZ = false;
+let axes = false;
+let ground = true;
 
 function init() {
-	var canvasWidth = 846;
-	var canvasHeight = 494;
+	let canvasWidth = 846;
+	let canvasHeight = 494;
 	// For grading the window is fixed in size; here's general code:
-	//var canvasWidth = window.innerWidth;
-	//var canvasHeight = window.innerHeight;
-	var canvasRatio = canvasWidth / canvasHeight;
+	//let canvasWidth = window.innerWidth;
+	//let canvasHeight = window.innerHeight;
+	let canvasRatio = canvasWidth / canvasHeight;
 
 	// RENDERER
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -56,9 +56,9 @@ function init() {
 // Supporting frame for the bird - base + legs + feet
 function createSupport() {
 
-	var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xF07020 } );
+	let cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xF07020 } );
 	// base
-	var cube;
+	let cube;
 	cube = new THREE.Mesh(
 		new THREE.CubeGeometry( 20+64+110, 4, 2*77 ), cubeMaterial );
 	cube.position.x = -45;	// (20+32) - half of width (20+64+110)/2
@@ -90,15 +90,15 @@ function createSupport() {
 
 // Body of the bird - body and the connector of body and head
 function createBody() {
-	var sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
-	var cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
+	let sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
+	let cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
 
 }
 
 // Head of the bird - head + hat
 function createHead() {
-	var sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
-	var cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
+	let sphereMaterial = new THREE.MeshLambertMaterial( { color: 0xA00000 } );
+	let cylinderMaterial = new THREE.MeshLambertMaterial( { color: 0x0000D0 } );
 
 }
 
@@ -120,11 +120,11 @@ function fillScene() {
 	scene = new THREE.Scene();
 	scene.fog = new THREE.Fog( 0x808080, 3000, 6000 );
 	// LIGHTS
-	var ambientLight = new THREE.AmbientLight( 0x222222 );
-	var light = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
+	let ambientLight = new THREE.AmbientLight( 0x222222 );
+	let light = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
 	light.position.set( 200, 400, 500 );
 
-	var light2 = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
+	let light2 = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
 	light2.position.set( -400, 200, -300 );
 
 	scene.add(ambientLight);
@@ -150,8 +150,8 @@ function fillScene() {
 }
 //
 function addToDOM() {
-	var container = document.getElementById('container');
-	var canvas = container.getElementsByTagName('canvas');
+	let container = document.getElementById('container');
+	let canvas = container.getElementsByTagName('canvas');
 	if (canvas.length>0) {
 		container.removeChild(canvas[0]);
 	}
@@ -164,7 +164,7 @@ function animate() {
 }
 
 function render() {
-	var delta = clock.getDelta();
+	let delta = clock.getDelta();
 	cameraControls.update(delta);
 	if ( effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes)
 	{
@@ -190,7 +190,7 @@ function setupGui() {
 		newAxes: axes
 	};
 
-	var gui = new dat.GUI();
+	let gui = new dat.GUI();
 	gui.add(effectController, "newGridX").name("Show XZ grid");
 	gui.add( effectController, "newGridY" ).name("Show YZ grid");
 	gui.add( effectController, "newGridZ" ).name("Show XY grid");
@@ -204,6 +204,6 @@ try {
 	addToDOM();
 	animate();
 } catch(e) {
-	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+	let errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
 	$('#container').append(errorReport+e);
 }
