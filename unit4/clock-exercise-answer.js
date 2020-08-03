b4292847f9683ea923b5fb4e50d5c3c43cd5ced7
Daniel P Pflager <daniel.pflager@gmail.com>
2020-08-02 20:11:05 -0700

    prepare for clock-exercise-answer

--- unit4/clock-exercise-answer.js
+++ unit4/clock-exercise-answer.js
@@ -91,11 +91,13 @@ function fillScene() {
 }
 
 function init() {
-	let canvasWidth = 846;
-	let canvasHeight = 494;
-	// For grading the window is fixed in size; here's general code:
-	//let canvasWidth = window.innerWidth;
-	//let canvasHeight = window.innerHeight;
+	document.body.style.margin = "0";
+	document.body.style.padding = "0";
+	document.body.style.overflow = "hidden";
+
+	let canvasWidth = document.documentElement.clientWidth;
+	let canvasHeight = document.documentElement.clientHeight;
+
 	let canvasRatio = canvasWidth / canvasHeight;
 
 	// RENDERER
