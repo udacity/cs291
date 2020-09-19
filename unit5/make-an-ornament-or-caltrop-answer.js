5110e6f4d0ab38d9039320baaf5d640733521769
Daniel P Pflager <daniel.pflager@gmail.com>
2020-09-18 17:33:56 -0700

    Answer

---
+++ unit5/make-an-ornament-or-caltrop-answer.js
@@ -0,0 +1,184 @@
+"use strict"; // good practice - see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+////////////////////////////////////////////////////////////////////////////////
+// Ornament axis/angle exercise: add three more cylinders to the ornament
+////////////////////////////////////////////////////////////////////////////////
+/*global THREE, Coordinates, document, window, dat*/
+
+let camera, scene, renderer;
+let cameraControls, effectController;
+let clock = new THREE.Clock();
+let bCube = true;
+let gridX = false;
+let gridY = false;
+let gridZ = false;
+let axes = true;
+let ground = false;
+
+function fillScene() {
+    scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0x808080, 2000, 4000);
+
+    // LIGHTS
+    let ambientLight = new THREE.AmbientLight(0x222222);
+
+    let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    light.position.set(200, 400, 500);
+
+    let light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    light2.position.set(-500, 250, -200);
+
+    scene.add(ambientLight);
+    scene.add(light);
+    scene.add(light2);
+
+    let cylinderMaterial = new THREE.MeshPhongMaterial({color: 0xD1F5FD, specular: 0xD1F5FD, shininess: 100});
+
+    // get two diagonally-opposite corners of the cube and compute the
+    // cylinder axis direction and length
+    let maxCorner = new THREE.Vector3(1, 1, 1);
+    let minCorner = new THREE.Vector3(-1, -1, -1);
+    // note how you can chain one operation on to another:
+    let cylAxis = new THREE.Vector3().subVectors(maxCorner, minCorner);
+    let cylLength = cylAxis.length();
+
+    // take dot product of cylAxis and up vector to get cosine of angle
+    cylAxis.normalize();
+    let theta = Math.acos(cylAxis.dot(new THREE.Vector3(0, 1, 0)));
+    // or just simply theta = Math.acos( cylAxis.y );
+
+    // YOUR CODE HERE
+    let cylinderGeo = new THREE.CylinderGeometry(0.2, 0.2, cylLength, 32);
+    for (let i = 0; i < 4; i++) {
+        let cylinder = new THREE.Mesh(cylinderGeo, cylinderMaterial);
+        let x = (i < 2) ? -1 : 1;
+        let z = (i % 2) ? -1 : 1;
+        let rotationAxis = new THREE.Vector3(x, 0, z);
+        rotationAxis.normalize();
+        cylinder.matrixAutoUpdate = false;
+        cylinder.matrix.makeRotationAxis(rotationAxis, theta);
+        scene.add(cylinder);
+    }
+
+}
+
+function drawHelpers() {
+    if (ground) {
+        Coordinates.drawGround({size: 100});
+    }
+    if (gridX) {
+        Coordinates.drawGrid({size: 100, scale: 1});
+    }
+    if (gridY) {
+        Coordinates.drawGrid({size: 100, scale: 1, orientation: "y"});
+    }
+    if (gridZ) {
+        Coordinates.drawGrid({size: 100, scale: 1, orientation: "z"});
+    }
+    if (axes) {
+        Coordinates.drawAllAxes({axisLength: 5, axisRadius: 0.01, axisTess: 50});
+    }
+
+    if (bCube) {
+        let cubeMaterial = new THREE.MeshLambertMaterial(
+            {color: 0xFFFFFF, opacity: 0.7, transparent: true});
+        let cube = new THREE.Mesh(
+            new THREE.CubeGeometry(2, 2, 2), cubeMaterial);
+        scene.add(cube);
+    }
+}
+
+function init() {
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+    document.body.style.overflow = "hidden";
+
+    let canvasWidth = document.documentElement.clientWidth;
+    let canvasHeight = document.documentElement.clientHeight;
+
+    let canvasRatio = canvasWidth / canvasHeight;
+
+    // RENDERER
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.setSize(canvasWidth, canvasHeight);
+    renderer.setClearColorHex(0xAAAAAA, 1.0);
+
+    let container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+
+    // CAMERA
+    camera = new THREE.PerspectiveCamera(30, canvasRatio, 1, 10000);
+    // CONTROLS
+    cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+    camera.position.set(-7, 7, 2);
+    cameraControls.target.set(0, 0, 0);
+
+}
+
+function addToDOM() {
+    let container = document.getElementById('container');
+    let canvas = container.getElementsByTagName('canvas');
+    if (canvas.length > 0) {
+        container.removeChild(canvas[0]);
+    }
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    let delta = clock.getDelta();
+    cameraControls.update(delta);
+
+    if (effectController.newCube !== bCube || effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes) {
+        bCube = effectController.newCube;
+        gridX = effectController.newGridX;
+        gridY = effectController.newGridY;
+        gridZ = effectController.newGridZ;
+        ground = effectController.newGround;
+        axes = effectController.newAxes;
+
+        fillScene();
+        drawHelpers();
+    }
+    renderer.render(scene, camera);
+}
+
+
+function setupGui() {
+
+    effectController = {
+
+        newCube: bCube,
+        newGridX: gridX,
+        newGridY: gridY,
+        newGridZ: gridZ,
+        newGround: ground,
+        newAxes: axes
+    };
+
+    let gui = new dat.GUI();
+    gui.add(effectController, "newCube").name("Show cube");
+    gui.add(effectController, "newGridX").name("Show XZ grid");
+    gui.add(effectController, "newGridY").name("Show YZ grid");
+    gui.add(effectController, "newGridZ").name("Show XY grid");
+    gui.add(effectController, "newGround").name("Show ground");
+    gui.add(effectController, "newAxes").name("Show axes");
+}
+
+try {
+    init();
+    fillScene();
+    setupGui();
+    drawHelpers();
+    addToDOM();
+    animate();
+} catch (e) {
+    let errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+    $('#container').append(errorReport + e);
+}
+
